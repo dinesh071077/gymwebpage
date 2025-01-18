b8e7a594d4b96f5a10f6e29f673c7f7a86e8756e
dinesh <dinesh071077@gmail.com>
2025-01-18 20:41:07 +0530

    Added new details to the code

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,7 +7,7 @@ function About() {
              <video src={aboutimg} autoPlay loop muted/>
         </div>
         <div className="about-text">
-            <h1>MORE ABOUT US</h1>
+            <h1>LEARN MORE ABOUT US</h1>
             <p>A gym isn’t just a place for exercise; it’s the place you go to unwind, socialize, and work out. The gym is a whole experience. Some of the most successful facilities have several gym features that contribute to the kind of member experience that drives retention and sales. </p>
               <a href='/Readmore' ><button>VIWE MORE</button></a>
         </div>
